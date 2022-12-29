9af7e268b984ff868fea8bb4c7a3c74806f7e2ab
Bedirhan Köse <bedirhan.kose05@gmail.com>
2022-12-29 23:46:51 +0100

    Second view controller created and connected.

---
+++ MapsApp/ListViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ListViewController.swift
+//  MapsApp
+//
+//  Created by Bedirhan Köse on 29.12.22.
+//
+
+import UIKit
+
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+    }
+    
+    @objc func addButtonTapped() {
+        performSegue(withIdentifier: "toMapsVC", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "test"
+        return cell
+    }
+    
+
+}
