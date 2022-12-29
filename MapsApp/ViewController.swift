31db40520242dbd00927e32446c1857f8fcc8189
Bedirhan Köse <bedirhan.kose05@gmail.com>
2022-12-29 21:49:51 +0100

    MapKit, CoreLocation

--- MapsApp/ViewController.swift
+++ MapsApp/ViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations[0].coordinate.latitude)
+        print(locations[0].coordinate.longitude)
     }
 
 
