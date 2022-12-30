4db65731efadc3029582188c2d2f733e78382dac
Bedirhan Köse <bedirhan.kose05@gmail.com>
2022-12-30 01:05:33 +0100

    observer has been added.

--- MapsApp/ListViewController.swift
+++ MapsApp/ListViewController.swift
@@ -29,7 +29,11 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
         getData()
     }
     
-    func getData(){
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name("NewPlaceHasBeenCreated"), object: nil)
+    }
+    
+    @objc func getData(){
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
         
